cae9fe368ee775dbdca6a9ad446ae861174f14ea
ZYLIII <elijahlzy115@gmail.com>
2023-10-26 16:11:39 +0800

    Integrate REST Web service to display course info on index page

--- index.php
+++ index.php
@@ -10,7 +10,33 @@
 
 <body>
 
-    <!-- Other content -->
+    <?php
+    $url = "https://titan.csit.rmit.edu.au/~e103884/wp/.services/.courses/";
+    $response = file_get_contents($url);
+    $courses = json_decode($response, true);
+    ?>
+
+    <div id="courses">
+        <h2>Courses</h2>
+        <table>
+            <tr>
+                <th>Course ID</th>
+                <th>Title</th>
+                <th>Credit Points</th>
+                <th>Career</th>
+                <th>Coordinator</th>
+            </tr>
+            <?php foreach ($courses as $course): ?>
+            <tr>
+                <td><?php echo htmlspecialchars($course['courseID']); ?></td>
+                <td><?php echo htmlspecialchars($course['title']); ?></td>
+                <td><?php echo htmlspecialchars($course['creditPoints']); ?></td>
+                <td><?php echo htmlspecialchars($course['career']); ?></td>
+                <td><?php echo htmlspecialchars($course['coordinator']); ?></td>
+            </tr>
+            <?php endforeach; ?>
+        </table>
+    </div>
 
     <footer>
         <div id="sitemap">
